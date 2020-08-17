4aa9b46133fe9c9531abd6eb5b9c42afdf36e98d
Florian Weich <hallo@florianweich.me>
2020-08-17 10:56:59 +0200

    Initial docs project setup

---
+++ nuxt.config.js
@@ -0,0 +1,15 @@
+import theme from '@nuxt/content-theme-docs';
+
+export default theme({
+  i18n: {
+    locales: () => [
+      {
+        code: 'de',
+        iso: 'de-DE',
+        file: 'de-DE.js',
+        name: 'Deutsch',
+      },
+    ],
+    defaultLocale: 'de',
+  },
+});
